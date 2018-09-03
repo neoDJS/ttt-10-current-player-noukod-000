c9e3cc57ae0dadf70c9a1160a873ba2d4e6e1c0c
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-03 14:57:18 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter_taken = 0
 
   board.each do |casier|
-    counter_taken += casier = "X" || casier = "O"? 1 : 0
+    counter_taken += casier == "X" || casier == "O"? 1 : 0
   end
  return counter_taken
 end
